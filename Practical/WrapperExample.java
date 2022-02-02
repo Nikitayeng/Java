c54cc812507e4964cb111a38986f57bfeaad7a70
Nikitayeng <53796671+Nikitayeng@users.noreply.github.com>
2022-02-02 14:42:39 +0530

    Add files via upload

---
+++ Practical/WrapperExample.java
@@ -0,0 +1,15 @@
+package nikita_code;
+
+public class WrapperExample {
+//Java program to convert primitive into objects  
+//Autoboxing example of int to Integer  
+ 
+public static void main(String args[]){  
+//Converting int into Integer  
+int a=20;  
+Integer i=Integer.valueOf(a);//converting int into Integer explicitly  
+Integer j=a;//autoboxing, now compiler will write Integer.valueOf(a) internally  
+
+System.out.println(a+" "+i+" "+j);  
+}
+}
