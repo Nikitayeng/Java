cc0e8d3c3f4fed969a6538ea5637750f1e6965a8
Nikitayeng <53796671+Nikitayeng@users.noreply.github.com>
2022-02-02 16:31:52 +0530

    Add files via upload

---
+++ Practical/ArrayExam.java
@@ -0,0 +1,12 @@
+package nikita_code;
+
+public class ArrayExam {
+
+	public static void main(String[] args) {
+		int a[]= {1,2,3,4,5};
+		for(int i=0;i<a.length;i++)
+			System.out.println(a[i]);
+		}
+
+
+}
