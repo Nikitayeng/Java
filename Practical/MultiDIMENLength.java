8af111bcd1a57443177ac9a7e28b50f1472a4c9d
Nikitayeng <53796671+Nikitayeng@users.noreply.github.com>
2022-02-03 14:56:19 +0530

    Add files via upload

---
+++ Practical/MultiDIMENLength.java
@@ -0,0 +1,14 @@
+package nikita_code;
+
+public class MultiDIMENLength {
+
+	public static void main(String[] args) {
+	
+		int[][] a = { {1,2,3},{4,5,6}, {7,8,9} };
+	     System.out.println("length of row l:"+a[0].length);
+	     System.out.println("length of row l:"+a[1].length);
+	     System.out.println("length of row l:"+a[2].length);
+	      
+	}
+
+}
