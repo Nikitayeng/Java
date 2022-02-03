aa4590099430f2f9528a2866163b772a29c57adf
Nikitayeng <53796671+Nikitayeng@users.noreply.github.com>
2022-02-03 15:59:50 +0530

    Add files via upload

---
+++ Assignment/Ass_driver5.java
@@ -0,0 +1,21 @@
+package nikita_code;
+
+public class Ass_driver5 {
+
+	public static void main(String[] args) {
+		
+		{ 
+		int [] intArr = new int [5]; 
+		for (int i = 0; i < intArr . length ; i ++) 
+		{
+		 intArr [ i ] = i * 2;
+		 }
+		 for (int i = 0; i < intArr . length ; i ++) 
+		{ 
+		System . out . print ( intArr [ i ] + ", ") ;
+		 }
+		 } 
+
+	}
+
+}
