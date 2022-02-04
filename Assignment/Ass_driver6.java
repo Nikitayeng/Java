0ce3e73c3447a91706b2fdc6baf3d3540d519e13
Nikitayeng <53796671+Nikitayeng@users.noreply.github.com>
2022-02-04 14:26:14 +0530

    Add files via upload

---
+++ Assignment/Ass_driver6.java
@@ -0,0 +1,22 @@
+package nikita_code;
+
+public class Ass_driver6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+		{
+		int arr[] = { 11, 22, 33 };
+		for (int i = 0; i < arr.length; i++)
+		System.out.print(arr[i] + " ");
+		System.out.println();
+		int arr2[] = new int[3];
+	    for (int i = 0; i < arr2.length; i++)
+		System.out.print(arr2[i] + " ");
+		}
+
+		}
+
+	}
+
+
