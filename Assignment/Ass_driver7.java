aa5066e6c311b8f52f55374e5224b63c46091b8c
Nikitayeng <53796671+Nikitayeng@users.noreply.github.com>
2022-02-04 14:42:05 +0530

    Add files via upload

---
+++ Assignment/Ass_driver7.java
@@ -0,0 +1,22 @@
+package nikita_code;
+
+public class Ass_driver7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+
+    	
+       	 String str[] = { "Geeks", "for", "Geeks" };
+       	 for (int i = 0; i < str.length; i++)
+            System.out.print(str[i]);
+    	}
+}
+
+
+
+
+
+	
+
+
