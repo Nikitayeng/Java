9f23d104d79e2dba9b3d7b245ccbb2b8b7a79bb8
Nikitayeng <53796671+Nikitayeng@users.noreply.github.com>
2022-02-06 23:20:21 +0530

    Add files via upload

---
+++ Practical/ImmutableDemo.java
@@ -0,0 +1,13 @@
+package nikita_code;
+
+public class ImmutableDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         String a="Nikita";
+         a.concat("Yengade");
+         a=a.concat("Yengade");
+         System.out.println(a);
+	}
+
+}
