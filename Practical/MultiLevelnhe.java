98f261f9697cc88e6329905eb13f42c935fdd70a
Nikitayeng <53796671+Nikitayeng@users.noreply.github.com>
2022-02-06 22:13:20 +0530

    Add files via upload

---
+++ Practical/MultiLevelnhe.java
@@ -0,0 +1,30 @@
+package nikita_code;
+
+class Animal {
+		void eat()
+		{
+			System.out.println("eating...");
+		}
+	}
+	class Dog extends Animal{
+		void bark()
+		{
+			System.out.println("barking...");
+		}
+	}
+	class BabyDog extends Dog{
+		void weep()
+		{
+			System.out.println("weeping...");
+		}
+	}
+	public class MultiLevelnhe {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     BabyDog b=new BabyDog();
+      b.weep();
+      b.bark();
+      b.eat();
+	}
+	}
