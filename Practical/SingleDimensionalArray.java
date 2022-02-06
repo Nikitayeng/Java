0bbd495ebfe260cafa4cd357b5a2a69aa2debfe4
Nikitayeng <53796671+Nikitayeng@users.noreply.github.com>
2022-02-06 23:07:21 +0530

    Add files via upload

---
+++ Practical/SingleDimensionalArray.java
@@ -0,0 +1,16 @@
+package nikita_code;
+public class SingleDimensionalArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ int a[]=new int[5];//declaring and instantiating an array
+ a[0]=10;//initialization
+ a[1]=20;
+ a[2]=30;
+ a[3]=40;
+ a[4]=50;
+ for(int i=0;i<a.length;i++)//Length is the property of array
+	 System.out.println(a[i]);
+	}
+
+}
