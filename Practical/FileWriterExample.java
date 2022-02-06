4924031b8ded316bba25d443cbf8721b945f7c38
Nikitayeng <53796671+Nikitayeng@users.noreply.github.com>
2022-02-06 23:50:36 +0530

    Add files via upload

---
+++ Practical/FileWriterExample.java
@@ -0,0 +1,31 @@
+package nikita_code;
+
+
+//package com.java.io;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FileWriterExample {
+
+  public static void main(String[] args) {
+      FileWriter writer = null;
+      try {
+          writer = new FileWriter("D:\\\\nikita\\\\test.txt");
+          writer.write("Writing Test data");
+          writer.flush();
+          System.out.println("Data written to File");
+      } catch (IOException ioe) {
+          ioe.printStackTrace();
+      } finally {
+          try {
+              if (writer != null) {
+                  writer.close();
+              }
+          } catch (IOException ioe) {
+
+          }
+      }
+  }
+
+}
