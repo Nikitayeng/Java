e4cd25d27f37ad91b10300198bf10d7a00e2d5c1
Nikitayeng <53796671+Nikitayeng@users.noreply.github.com>
2022-02-06 22:17:38 +0530

    Add files via upload

---
+++ Practical/Hierarchical_INHE.java
@@ -0,0 +1,30 @@
+package nikita_code;
+class Animal1{
+	void eat()
+	{
+		System.out.println("eating...");
+	}
+}
+class Dog1 extends Animal1{
+	void bark()
+	{
+		System.out.println("barking...");
+	}
+}
+class Cat extends Animal1{
+	void meow()
+	{
+		System.out.println("meowing...");
+	}
+}
+public class Hierarchical_INHE {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        Cat c=new Cat();
+       c.meow();
+        c.eat();
+        //c.bark();
+	}
+
+}
