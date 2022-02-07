99bc828df28846f4e85d5b5344e871de9b1059a8
Nikitayeng <53796671+Nikitayeng@users.noreply.github.com>
2022-02-07 08:57:38 +0530

    Add files via upload

---
+++ Assignment/Alligator.java
@@ -0,0 +1,15 @@
+package nikita_code;
+
+public class Alligator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+			int []x[] = {{1,2}, {3,4,5}, {6,7,8,9}}; 
+			int [][]y = x; 
+			System.out.println(y[2][1]);
+			 } 
+
+	}
+
+
