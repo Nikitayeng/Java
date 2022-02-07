e2483926e2a58c2cb6161d0cc851c20772987ff3
Nikitayeng <53796671+Nikitayeng@users.noreply.github.com>
2022-02-07 13:45:19 +0530

    Add files via upload

---
+++ Assignment/Ass10.java
@@ -0,0 +1,27 @@
+package nikita_code;
+
+class A 
+		{ int i; 
+		void display() 
+		 { 
+		 System.out.println(i);
+		 }
+		} 
+		class B extends A
+		 { int j;  
+		void display()
+		  {
+		  System.out.println(j); 
+		  }
+		} 
+		class Ass10
+		{ 
+		public static void main(String args[]) 
+		{
+			B obj=new B();
+		   obj.i=1;
+		   obj.j=2; 
+		   obj.display();
+			 }
+		}
+
