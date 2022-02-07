d2ffecc493695f593df1b2d363384524be75b24b
Nikitayeng <53796671+Nikitayeng@users.noreply.github.com>
2022-02-07 08:53:12 +0530

    Add files via upload

---
+++ Assignment/ass8_.java
@@ -0,0 +1,14 @@
+package nikita_code;
+
+public class ass8_ {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		 int arr[] = {1, 2, 3, 4, 5}; 
+		for ( int i = 0; i < arr.length - 2; ++i)
+		 System.out.println(arr[i] + " "); 
+		} 
+
+	}
+
