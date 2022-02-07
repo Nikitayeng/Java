de3ad8b85ff4525c45f39dcdd3bfd3b5bb3138b8
Nikitayeng <53796671+Nikitayeng@users.noreply.github.com>
2022-02-07 08:39:43 +0530

    Add files via upload

---
+++ Assignment/Evaluate.java
@@ -0,0 +1,18 @@
+package nikita_code;
+
+public class Evaluate {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+		 {
+		 int arr[] = new int[] {0 , 1, 2, 3, 4, 11, 6, 7, 8, 9}; 
+		 int n = 6;
+		 n = arr[arr[n] / 2];
+		 
+		 System.out.println(arr[n] / 2); 
+		} }
+
+	}
+
+
