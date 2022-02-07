7d9b83388fb61a251031bc7547768cac2329c38a
Nikitayeng <53796671+Nikitayeng@users.noreply.github.com>
2022-02-07 08:41:38 +0530

    Add files via upload

---
+++ Assignment/LeftShiftOperator.java
@@ -0,0 +1,17 @@
+package nikita_code;
+
+public class LeftShiftOperator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+		byte x = 64; 
+		int i; 
+		byte y; 
+		i = x << 2;
+		 y = (byte) (x << 2) ;
+		System.out.print(i + " " + y); 
+		} }
+
+
+
