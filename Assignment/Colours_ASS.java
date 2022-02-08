0584f7993c759830f90b374c759bce173bd6780e
Nikitayeng <53796671+Nikitayeng@users.noreply.github.com>
2022-02-08 22:32:30 +0530

    Add files via upload

---
+++ Assignment/Colours_ASS.java
@@ -0,0 +1,19 @@
+package Collection;
+import java.util.ArrayList;
+
+public class Colours_ASS {
+   public static void main(String [] args) {
+	  
+		        ArrayList<String> color = new ArrayList<String>();
+		        color.add("Pink");
+		        color.add("Blue");
+		        color.add("Purple");
+		        color.add("White");
+		        System.out.print(color);
+		      
+			      }
+		    }
+		
+	
+
+
