1a868cba1c602914a19fcabec795c15056f2a465
Nikitayeng <53796671+Nikitayeng@users.noreply.github.com>
2022-02-08 20:22:27 +0530

    Add files via upload

---
+++ Practical/Collection/Vectors.java
@@ -0,0 +1,18 @@
+package Collection;
+import java.util.*;
+public class Vectors {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Vector<String> v=new Vector<String>();  
+		v.add("Ayush");  
+		v.add("Amit");  
+		v.add("Ashish");  
+		v.add("Garima");  
+		Iterator<String> itr=v.iterator();  
+		while(itr.hasNext()){  
+		System.out.println(itr.next());  
+		}
+	}
+
+}
