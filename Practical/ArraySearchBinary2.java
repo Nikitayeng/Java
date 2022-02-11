d77ac9300fd511d876045e7c52ffa42be7634170
Nikitayeng <53796671+Nikitayeng@users.noreply.github.com>
2022-02-11 18:11:14 +0530

    Add files via upload

---
+++ Practical/ArraySearchBinary2.java
@@ -0,0 +1,50 @@
+package collections;
+
+import java.util.Scanner;
+import java.util.Arrays;
+class ArraySearchBinary2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int counter,num,item,array[],first,last,middle;
+Scanner a =new Scanner(System.in);
+System.out.println("Enter number of element: ");
+num = a.nextInt();
+array= new int[num];
+
+
+System.out.println("Enter "+ num +  "integers");
+		for (counter = 0; counter < num; counter++)
+	          array[counter] = a.nextInt();
+
+	      System.out.println("Enter the search value:");
+	      item = a.nextInt();
+	      first = 0;
+	      last = num - 1;
+	      middle = (first + last)/2;
+
+	      while( first <= last )
+	      {
+	         if ( array[middle] < item )
+	           first = middle + 1;
+	         else if ( array[middle] == item )
+	         {
+	           System.out.println(item + " found at location " + (middle + 1) + ".");
+	           break;
+	         }
+	         else
+	         {
+	             last = middle - 1;
+	         }
+	         middle = (first + last)/2;
+	      }
+	      if ( first > last )
+	          System.out.println(item + " is not found.\n");
+	      
+	}
+
+	private static int array() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+}
