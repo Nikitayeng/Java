bcfcc460c7db580267eb4002fceae69b34b4f754
Nikitayeng <53796671+Nikitayeng@users.noreply.github.com>
2022-02-11 18:17:39 +0530

    Add files via upload

---
+++ Practical/hashset123.java
@@ -0,0 +1,19 @@
+package collections;
+import java.util.*;  
+public class hashset123 {
+
+public static void main(String args[]){  
+//Creating and adding elements  
+TreeSet<String> set=new TreeSet<String>();  
+set.add("Ravi");  
+set.add("Vijay");  
+set.add("Ravi");  
+set.add("Ajay");  
+//traversing elements  
+Iterator<String> itr=set.iterator();  
+while(itr.hasNext()){  
+System.out.println(itr.next());  
+}  
+}  
+}
+
