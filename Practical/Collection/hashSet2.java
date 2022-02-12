c2eee4b3fb6fc76fe8baa5abcaba5116df671ab7
Nikitayeng <53796671+Nikitayeng@users.noreply.github.com>
2022-02-12 12:54:37 +0530

    Add files via upload

---
+++ Practical/Collection/hashSet2.java
@@ -0,0 +1,20 @@
+package collections;
+import java.util.*;
+public class hashSet2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		HashSet<String> set=new HashSet();  
+        set.add("One");    
+        set.add("Two");    
+        set.add("Three");   
+        set.add("Four");  
+        set.add("Five");  
+        Iterator<String> i=set.iterator();  
+        while(i.hasNext())  
+        {  
+        System.out.println(i.next());  
+        }
+	}
+
+}
