6a1b2171a286e3c2731094636da6ef2aa0c76b16
Nikitayeng <53796671+Nikitayeng@users.noreply.github.com>
2022-02-12 13:17:05 +0530

    Update equhash.java

--- Practical/Collection/equhash.java
+++ Practical/Collection/equhash.java
@@ -4,8 +4,8 @@ class equhash {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		EandH g1 =new EandH("a",1);
-		EandH g2 =new EandH("b",1);	
+			EqualAndHashCode g1 =new EqualAndHashCode("a",1);
+		EqualAndHashCode g2 =new EqualAndHashCode("b",1);	
 				if(g1.hashCode() == g2.hashCode())
 		        {
 		  
