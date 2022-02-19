51b3556208910c888002f5e7684013dceb3b2932
Nikitayeng <53796671+Nikitayeng@users.noreply.github.com>
2022-02-19 22:17:15 +0530

    Add files via upload

---
+++ Assignment/String/ASS_string6.java
@@ -0,0 +1,17 @@
+package string;
+
+public class ASS_string6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str = "The Quick BroWn FoX!";
+
+        // Convert the above string to all lowercase.
+        String lowerStr = str.toLowerCase();
+
+        // Display the two strings for comparison.
+        System.out.println("Original String: " + str);
+        System.out.println("String in lowercase: " + lowerStr);
+	}
+
+}
