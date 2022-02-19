a4198e7a4a53fdba00d6ef47b66f0d3312aa6257
Nikitayeng <53796671+Nikitayeng@users.noreply.github.com>
2022-02-19 13:40:38 +0530

    Add files via upload

---
+++ Practical/MavenProject/My_Name/App.java
@@ -0,0 +1,13 @@
+package nikita.name;
+
+/**
+ * Hello, My NAME IS NIKITA
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello, My Name is Nikita" );
+    }
+}
