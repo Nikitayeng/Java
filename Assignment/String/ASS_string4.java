207fd11d8e89c4c971e90f872aa58628d5cb091c
Nikitayeng <53796671+Nikitayeng@users.noreply.github.com>
2022-02-19 22:15:49 +0530

    Add files via upload

---
+++ Assignment/String/ASS_string4.java
@@ -0,0 +1,10 @@
+package string;
+public class ASS_string4 {
+
+	public static void main(String[] args) {
+		String str = "example.com";
+	    // Get the length of str.
+	    int len = str.length();
+	    System.out.println("The string length of '"+str+"' is: "+len);
+	}
+}
