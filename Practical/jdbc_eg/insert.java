2425411ab7ab571348f2e657e7f9e5f824ddee5c
Nikitayeng <53796671+Nikitayeng@users.noreply.github.com>
2022-02-28 23:19:35 +0530

    Add files via upload

---
+++ Practical/jdbc_eg/insert.java
@@ -0,0 +1,32 @@
+package nikita.jdbcc_eg;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class insert {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try {
+	 		   Class.forName("com.mysql.cj.jdbc.Driver");
+	 		   Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/core","root","Nikita1");
+	 		   Statement st=con.createStatement();
+	 		   st.executeUpdate("insert into student values(9, 'sweety' , 49)");
+	 		   System.out.println("inserted successfully into table");
+	 		   ResultSet rs=st.executeQuery("select * from student");
+	 		   while(rs.next()) {
+	 			   System.out.println(rs.getInt(1) + " " + rs.getString(2) + " " + rs.getInt(3));
+	 		   } 
+	 		   con.close();
+	 		   }
+		 catch(Exception e) {
+	 		   System.out.println(e);
+	}
+
+}
+}
+	
+
+		
