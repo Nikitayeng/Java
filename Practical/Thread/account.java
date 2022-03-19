3a7592b45e1fa02ef312b9439430453e9560e7df
Nikitayeng <53796671+Nikitayeng@users.noreply.github.com>
2022-03-19 17:12:38 +0530

    Update account.java

--- Practical/Thread/account.java
+++ Practical/Thread/account.java
@@ -3,7 +3,7 @@ package com;
 public class account {
 
 	
-		public class Account {
+		
 			  
 			private int balance=600;
 			public int getBalance() {
@@ -14,6 +14,6 @@ public class account {
 			}
 		}
 		
-	}
+	
 
 
